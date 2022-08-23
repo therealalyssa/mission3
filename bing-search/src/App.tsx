860a16cab7c06c57731d4f52d98b95fb948ab50a
ranjurave <ranjurave@gmail.com>
2022-08-24 01:40:00 +1200

    react typescript added

---
+++ bing-search/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SearchBox from './SearchBox';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBox></SearchBox>
+    </div>
+  );
+}
+
+export default App;
---
+++ bing-search/src/SearchBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchBox() {
+  return (
+    <div>
+        <input></input>
+        <input type='submit'></input>
+    </div>
+  )
+}
+
+export default SearchBox
